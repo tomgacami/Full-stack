b5b396c4ef4be5cc958863e4fce49f85ffa1c6e0
tom <tomgacami@gmail.com>
2026-03-23 10:13:20 -0300

    Exercise 2.5 step 10

--- part2/courseinfo/courseinfo/src/App.jsx
+++ part2/courseinfo/courseinfo/src/App.jsx
@@ -1,45 +1,5 @@
 
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header title={course.name}/>
-            <Content parts={course.parts}/>
-            <Total parts={course.parts}/>
-        </div>
-    )
-}
-
-const Total = ({parts}) => {
-
-    const totalExercises = parts.reduce((total, actual) => total + actual.exercises, 0)
-    return(
-        <p><strong>Total of {totalExercises} exercises</strong></p>
-    )
-
-}
-
-const Content = ({parts}) => {
-    return(
-            <div>
-                {parts.map(part =>
-                    <Part key={part.id} part={part}/>
-                )}
-            </div>
-    )
-}
-
-const Part = ({part}) => {
-            return(
-                <p>{part.name + ' '} {part.exercises}</p>
-            )
-}
-
-const Header = ({title}) =>{
-    return(
-        <h1>{title}</h1>
-    )
-}
-
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
