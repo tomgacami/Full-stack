15e88700cd96bddab8237a74201ad217bf089165
tom <tomgacami@gmail.com>
2026-03-24 20:35:29 -0300

    Exercise 2.9 step 4

--- part2/agenda/agenda/src/App.jsx
+++ part2/agenda/agenda/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+  const [persons, setPersons] = useState(
+      [
+          { name: 'Arto Hellas', number: '040-123456', id: 1 },
+          { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+          { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+          { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+      ]
+  )
+
   const [newName, setNewName] = useState('')
   const [newNumber , setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
         const personObject = {
           name: newName,
-          number:newNumber
+          number:newNumber,
+            id: persons.length + 1
         }
 
         if (persons.some((person) => newName === person.name)) {
@@ -30,12 +40,31 @@ const App = () => {
   const handleNumberAdd = (event) => {
       // console.log('Number event: ', event.target.value)
       setNewNumber(event.target.value)
-
   }
+
+  const handleNameFilter = (event) => {
+      // console.log(event.target.value)
+      setNameFilter(event.target.value)
+      // const nameFilterPerson = persons.filter(function(person) {
+      //     return person.name.toLowerCase().includes(nameFilter.toLowerCase()) || person.name.toUpperCase().includes(nameFilter.toUpperCase())
+      // })
+      // console.log('Nombre filtrado: ', nameFilter)
+      // console.log('Filter: ', nameFilterPerson)
+  }
+
+  const personsToShow = !nameFilter
+    ? persons
+      :persons.filter(person =>
+      person.name.toLowerCase().includes(nameFilter.toLowerCase())
+      )
 
   return (
       <div>
         <Title title='Phonebook'/>
+          <div>
+              filter show with <input value={nameFilter}
+                                      onChange={handleNameFilter}/>
+          </div>
         <form onSubmit={addPerson}>
           <div>
             name: <input
@@ -55,11 +84,26 @@ const App = () => {
         </form>
 
         <Title title='Numbers'></Title>
-        <div>
-          {persons.map(person =>
-              <Person key={person.name} person={person}/>
-          )}
-        </div>
+        {/*  <div>*/}
+        {/*      {persons.filter(person =>{*/}
+        {/*          if (!nameFilter) return true*/}
+        {/*          if (person.name.toLowerCase().includes(nameFilter.toLowerCase())*/}
+        {/*              // || person.name.toUpperCase().includes(nameFilter.toUpperCase())*/}
+        {/*          )*/}
+        {/*          {*/}
+        {/*              return true*/}
+        {/*          }*/}
+        {/*      })*/}
+        {/*          .map(person => (*/}
+        {/*          <Person key={person.key} person={person}/>*/}
+        {/*          ))}*/}
+        {/*</div>*/}
+          <div>
+              {personsToShow.map(person =>
+                  <Person key={person.id} person={person}/>
+              )}
+
+          </div>
       </div>
   )
 }
