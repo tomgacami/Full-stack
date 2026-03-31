c591dcbe13746d495af3eca70e18974221865362
tom <tomgacami@gmail.com>
2026-03-30 21:02:39 -0300

    Exercise 2.11 step 6

--- part2/agenda/agenda/src/App.jsx
+++ part2/agenda/agenda/src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import MainTitle from "./components/MainTitle.jsx";
 import Persons from "./components/PersonsSection.jsx";
 import FilterByName from "./components/FilterByPersonName.jsx";
 import PersonForm from "./components/PersonForm.jsx";
+import axios from "axios";
 
 
 
 
 const App = () => {
 
-  const [persons, setPersons] = useState(
-      [
-          { name: 'Arto Hellas', number: '040-123456', id: 1 },
-          { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-          { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-          { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-      ]
-  )
+  const [persons, setPersons] = useState([])
+
+    useEffect(() => {
+        // console.log('Effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then((res) => {
+                // console.log('Response: ', res)
+                setPersons(res.data)
+            })
+    }, [])
+
+    // console.log('Render', persons.length, 'persons')
+
 
   const [newName, setNewName] = useState('')
   const [newNumber , setNewNumber] = useState('')
--- part2/agenda/agenda/src/main.jsx
+++ part2/agenda/agenda/src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// import axios from "axios";
 
-createRoot(document.getElementById('root')).render(
+
+// const promise = axios.get('http://localhost:3001/persons')
+// promise.then(res => {
+//     console.log('Presponse: ', res)
+//     console.log('Promise: ', promise)
+// })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
+
