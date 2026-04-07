49f51b5318ca7da0731c609df34f0c90b7690c55
tom <tomgacami@gmail.com>
2026-04-07 17:43:17 -0300

    Exercise 2.19 step 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,14 +2,17 @@ import {useEffect, useState} from 'react'
 import CountryForm from '../src/components/CountryForm.jsx'
 import countryServices from './services/countries.js'
 import CountryList from "./components/CountryList.jsx";
+import Country from "./components/Country.jsx";
 
 function App() {
 
     const [inputCountry, setInputCountry] = useState('')
     const [countriesList, setCountriesList] = useState([])
+    const [selectedCountry, setSelectedCountry] = useState(null)
 
   const handleCountryChange = (event) =>{
     setInputCountry(event.target.value)
+      setSelectedCountry(null)
   }
 
   useEffect(() =>{
@@ -33,7 +36,9 @@ function App() {
       <CountryForm
           inputCountry={inputCountry}
           onChange={handleCountryChange}/>
-      <CountryList countriesToDisplay={countriesToDisplay} inputCountry={inputCountry}/>
+      <CountryList countriesToDisplay={countriesToDisplay} inputCountry={inputCountry}
+                   setSelectedCountry={setSelectedCountry}/>
+        {selectedCountry && <Country countryData={selectedCountry}/>}
     </>
   )
 }
--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -1,24 +1,28 @@
 import Country from "./Country.jsx";
 
-const CountryList = ({countriesToDisplay, inputCountry}) => {
+const CountryList = ({countriesToDisplay, inputCountry, setSelectedCountry}) => {
 
     if (!inputCountry){
         return null
     }
-        if (countriesToDisplay.length > 10) {
-            return(
-                <div>Too many matches, specify another filter</div>
+
+    if (countriesToDisplay.length > 10) {
+        return(
+            <div>Too many matches, specify another filter</div>
+        )
+    } else if (countriesToDisplay.length > 1) {
+        return(
+            countriesToDisplay.map(country =>
+            <div key={country.name.common}>{country.name.common}
+                <button onClick={() => setSelectedCountry(country)}>Show</button>
+            </div>
             )
-        } else if (countriesToDisplay.length > 1) {
-            return(
-                countriesToDisplay.map(country =>
-                <div key={country.name.common}>{country.name.common}</div> )
-            )
-        } else {
-            return (
-                <Country countryData={countriesToDisplay[0]}/>
-            )
-        }
+        )
+    } else {
+        return (
+            <Country countryData={countriesToDisplay[0]}/>
+        )
+    }
 }
 
 export default CountryList
